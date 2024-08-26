480fdee10195badec9db3be436329b48468700f9
Tangeyo <82197718+Tangeyo@users.noreply.github.com>
2024-08-26 16:39:44 -0700

    Fix useref scope

--- src/components/CodebaseControls.jsx
+++ src/components/CodebaseControls.jsx
@@ -1,46 +1,39 @@
-import { useRef } from 'react';
+import { useRef } from "react";
 
 export default function CodebaseControls() {
+  const codebaseURLInput = useRef(null);
+  // Download a codebase
+  async function downloadCodebase() {
+    const response = await fetch(`${process.env.URL}/api/download`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ url: codebaseURLInput.current.value }),
+    });
 
-    // Download a codebase
-    async function downloadCodebase() {
-        const codebaseURLInput = useRef(null);
-    
-        const response = await fetch(`${process.env.URL}/api/download`, {
-            method: "POST",
-            headers: {
-                "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ "url": codebaseURLInput.current.value }),
-        });
-    
-        const result = await response.json();
-        if (result.error) {
-            alert(result.error);
-        } else {
-            alert("Codebase downloaded and cached");
-        }
+    const result = await response.json();
+    if (result.error) {
+      alert(result.error);
+    } else {
+      alert("Codebase downloaded and cached");
     }
+  }
 
-    // Delete the existing codebase
-    async function deleteCodebase() {
-        const response = await fetch(`${apiURL}/api/delete`, {
-            method: "GET"
-        });
-    
-        const result = await response.json();
-        if (result.error) {
-            alert(result.error);
-        } else {
-            alert("Codebase successfully deleted");
-        }
-    }
-
-    return (
-        <div className="flex">
-            <input type="text" ref={ codebaseURLInput } id="codebase-url" placeholder="Enter codebase URL (must be a link to download a .zip file)"></input>
-            <button id="download-button" onClick={ downloadCodebase }>Download</button>
-            <button id="delete-button" onClick={ deleteCodebase }>Remove</button>
-        </div>
-    );
+  return (
+    <div className="flex">
+      <input
+        type="text"
+        ref={codebaseURLInput}
+        id="codebase-url"
+        placeholder="Enter codebase URL (must be a link to download a .zip file)"
+      ></input>
+      <button id="download-button" onClick={downloadCodebase}>
+        Download
+      </button>
+      <button id="delete-button" onClick={deleteCodebase}>
+        Remove
+      </button>
+    </div>
+  );
 }
